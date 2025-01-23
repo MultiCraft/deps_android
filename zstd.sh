5828af126d0d4decd8467c060262a9ce5539c0a7
Maksym H. <Maksym48@pm.me>
2025-01-23 17:44:41 +0100

    Update zstd.sh

--- zstd.sh
+++ zstd.sh
@@ -8,23 +8,28 @@ mkdir -p output/zstd/lib/$TARGET_ABI
 mkdir -p deps; cd deps
 
 if [ ! -d zstd-src ]; then
-	git clone -b v$ZSTD_VERSION --depth 1 https://github.com/facebook/zstd.git zstd-src
-	mkdir zstd-src/build/cmake/builddir
+	if [ ! -f "zstd-v$ZSTD_VERSION.tar.gz" ]; then
+		wget -O zstd-v$ZSTD_VERSION.tar.gz https://github.com/facebook/zstd/archive/refs/tags/v$ZSTD_VERSION.tar.gz
+	fi
+	tar -xzf zstd-v$ZSTD_VERSION.tar.gz
+	mv zstd-$ZSTD_VERSION zstd-src
 fi
 
+mkdir -p zstd-src/build/cmake/builddir
 cd zstd-src/build/cmake/builddir
 
-cmake .. -DANDROID_STL="c++_static" -DANDROID_NATIVE_API_LEVEL="$NATIVE_API_LEVEL" \
+cmake .. -DANDROID_STL="c++_static" \
+	-DANDROID_NATIVE_API_LEVEL="$NATIVE_API_LEVEL" \
+	-DANDROID_ABI="$ANDROID_ABI" \
+	-DANDROID_PLATFORM="$API" \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DZSTD_MULTITHREAD_SUPPORT=ON \
+	-DCMAKE_C_FLAGS_RELEASE="$CFLAGS" \
+	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
+	-DZSTD_MULTITHREAD_SUPPORT=OFF \
 	-DZSTD_BUILD_TESTS=OFF \
 	-DZSTD_BUILD_PROGRAMS=OFF \
 	-DZSTD_BUILD_STATIC=ON \
-	-DZSTD_BUILD_SHARED=OFF \
-	-DANDROID_ABI="$ANDROID_ABI" \
-	-DANDROID_PLATFORM="$API" \
-	-DCMAKE_C_FLAGS_RELEASE="$CFLAGS" \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake"
+	-DZSTD_BUILD_SHARED=OFF
 
 cmake --build . -j
 
