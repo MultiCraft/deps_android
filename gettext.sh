9c14e3ef2f8003eb12bc05f6c204e498cdbce749
Maksym H. <Maksym48@pm.me>
2025-07-02 18:37:23 +0200

    Bumo libpng, libcurl, gettext and SDL2 versions

--- gettext.sh
+++ gettext.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 . ./sdk.sh
-GETTEXT_VERSION=0.24.1
+GETTEXT_VERSION=0.25
 
 mkdir -p output/gettext/lib/$TARGET_ABI
 mkdir -p deps; cd deps
 
 if [ ! -d gettext-src ]; then
 	if [ ! -f "gettext-$GETTEXT_VERSION.tar.xz" ]; then
-		wget https://ftp.gnu.org/pub/gnu/gettext/gettext-$GETTEXT_VERSION.tar.xz
+		wget https://fossies.org/linux/misc/gettext-$GETTEXT_VERSION.tar.xz
 	fi
 	tar -xaf gettext-$GETTEXT_VERSION.tar.xz
 	mv gettext-$GETTEXT_VERSION gettext-src
--- libpng.sh
+++ libpng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 . ./sdk.sh
-PNG_VERSION=1.6.48
+PNG_VERSION=1.6.49
 
 mkdir -p output/libpng/lib/$TARGET_ABI
 mkdir -p deps; cd deps
