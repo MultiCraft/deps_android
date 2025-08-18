13a9af90ff56afde9de05de895142312d62d689c
Maksym H. <Maksym48@pm.me>
2025-08-18 12:22:20 +0200

    Fix merge?

--- libSDL.sh
+++ libSDL.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-SDL_VERSION=2.32.8
+SDL_VERSION=3.2.8
 
 . ./sdk.sh
 
-mkdir -p output/sdl2/lib/$TARGET_ABI
+mkdir -p output/sdl3/lib/$TARGET_ABI
 mkdir -p deps; cd deps
 
-if [ ! -d sdl2-src ]; then
+if [ ! -d sdl3-src ]; then
     if [ ! -f "release-$SDL_VERSION.tar.gz" ]; then
 	   wget https://github.com/libsdl-org/SDL/archive/release-$SDL_VERSION.tar.gz
     fi
-	tar -xaf release-$SDL_VERSION.tar.gz
-	mv SDL-release-$SDL_VERSION sdl2-src
+	tar -xzf release-$SDL_VERSION.tar.gz
+	mv SDL-release-$SDL_VERSION sdl3-src
 fi
 
-cd sdl2-src
+cd sdl3-src
 
 mkdir -p build; cd build
 
@@ -27,17 +27,18 @@ cmake .. -DANDROID_STL="c++_static" \
     -DCMAKE_C_FLAGS="$CFLAGS" \
     -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
     -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
-    -DLIBTYPE=STATIC
+    -DSDL_STATIC=1 \
+    -DSDL_SHARED=0
 
 cmake --build . -j
 
 DESTDIR="$(pwd)/install" cmake --install .
 
 # update `include` folder
-rm -rf ../../../output/sdl2/include/
-cp -r ../include ../../../output/sdl2/include
+rm -rf ../../../output/sdl3/include/
+cp -r ../include ../../../output/sdl3/include
 # update lib
-rm -rf ../../../output/sdl2/lib/$TARGET_ABI/libSDL2.a
-cp -r libSDL2.a ../../../output/sdl2/lib/$TARGET_ABI/libSDL2.a
+rm -rf ../../../output/sdl3/lib/$TARGET_ABI/libSDL3.a
+cp -r libSDL3.a ../../../output/sdl3/lib/$TARGET_ABI/libSDL3.a
 
 echo "libSDL build successful"
